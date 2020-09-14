ec9a867bb3d1c8e7a8fa4a65434686ff0088abbb
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-09-13 22:57:25 -0300

    Classificando E-mails, Animais e Muito Mais

---
+++ classificacao.py
@@ -0,0 +1,31 @@
+from sklearn.naive_bayes import MultinomialNB
+
+# [é gordinho? tem perninha curta?, faz auau?]
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro1 = [1, 1, 1]
+cachorro2 = [0, 1, 1]
+cachorro3 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
+
+modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
+teste = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, -1]
+resultado = modelo.predict(teste)
+diferencas = resultado - marcacoes_teste
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+print(resultado)
+print(diferencas)
+print(taxa_de_acerto)
+print('\nFim!')
