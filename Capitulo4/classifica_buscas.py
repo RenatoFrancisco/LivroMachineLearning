39a48b61c0d77519be4194f3ed406eb0613301ae
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-09-19 16:29:54 -0300

    4.1 Implementando o Algoritmo Base

---
+++ Capitulo4/classifica_buscas.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+df = pd.read_csv('buscas.csv')
+x_df = df[['home', 'busca', 'logado']]
+y_df = df['comprou']
+xdummies_df = pd.get_dummies(x_df)
+ydummies_df = y_df
+
+x = xdummies_df.values
+y = ydummies_df.values
+
+acerto_de_um = sum(y)
+acerto_de_zero = len(y) - acerto_de_um
+taxa_de_acerto_base = 100.0 * max(acerto_de_um, acerto_de_zero) / len(y)
+print('Taxa de acerto base: %f' % taxa_de_acerto_base)
+
+porcetagem_treino = 0.9
+
+tamanho_de_treino = int(porcetagem_treino * len(y))
+tamanho_de_teste = len(y) - tamanho_de_treino
+
+treino_dados = x[:tamanho_de_treino]
+treino_marcacoes = y[:tamanho_de_treino]
+
+teste_dados = x[-tamanho_de_teste:]
+teste_marcacoes = y[-tamanho_de_teste:]
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
---
+++ Capitulo4/dados.py
@@ -0,0 +1,34 @@
+import csv
+
+def carregar_acessos():
+    x = []
+    y = []
+
+    arquivo = open('acesso.csv', 'r')
+    leitor = csv.reader(arquivo)
+    next(leitor)
+
+    for home, como_funciona, contato, comprou in leitor:
+        dado = [int(home), int(como_funciona), int(contato)]
+        x.append(dado)
+        y.append(int(comprou))
+
+    return x, y
+
+def carregar_buscas():
+    x = []
+    y = []
+
+    arquivo = open('buscas.csv', 'r')
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+
+    for home, busca, logado, comprou in leitor:
+        dado = ([int(home), busca, int(logado)])
+        x.append(dado)
+        y.append(int(comprou))
+
+    return x, y
+
+
