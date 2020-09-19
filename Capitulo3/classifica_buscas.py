0445d56cef036c9eb86fa8adc37c3a604c6b0850
renatofrancisco.s <renatofrancisco.s@gmail.com>
2020-09-19 16:01:15 -0300

    3.1 Instalando o Pandas

--- Capitulo3/classifica_buscas.py
+++ Capitulo3/classifica_buscas.py
@@ -1,4 +1,35 @@
-from dados import carregar_buscas
+import pandas as pd
 
-x, y = carregar_buscas();
-print(y)
+df = pd.read_csv('buscas.csv')
+x_df = df[['home', 'busca', 'logado']]
+y_df = df['comprou']
+xdummies_df = pd.get_dummies(x_df)
+ydummies_df = y_df
+
+x = xdummies_df.values
+y = ydummies_df.values
+
+porcetagem_treino = 0.9
+
+tamanho_de_treino = int(porcetagem_treino * len(y))
+tamanho_de_teste = len(y) - tamanho_de_treino
+
+treino_dados = x[:tamanho_de_treino]
+treino_marcacoes = y[:tamanho_de_treino]
+
+teste_dados = x[-tamanho_de_teste:]
+teste_marcacoes = y[-tamanho_de_teste:]
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
